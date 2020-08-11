6ce885d98f3c6a2f7a56b54c00c2b8735b014a97
Hector Gonzalez <hectarek13@gmail.com>
2020-08-11 14:49:16 -0400

    added structural divs to app.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Recipes from './Recipes';
+import Recipe from './Recipe';
+import Ingredients from './Ingredients';
+
+function App() {
+  return (
+    <div className="App container">
+      
+      <div className="row">
+        <Header/>
+      </div>
+
+      <div className="row">
+        <div className="col-md-3">
+          <Recipes/>
+        </div>
+        <div className="col-md-6">
+          <Recipe />
+        </div>
+        <div className="col-md-3">
+          <Ingredients/>
+        </div>
+      </div>
+
+      <div className="row">
+        <Footer/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+// this is a test
