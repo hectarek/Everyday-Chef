337d0cf9a7dab58dafc3acfedb5370251c21faa6
ashwini82 <ashwiniatharv1982@gmail.com>
2020-08-12 10:07:02 -0400

    adding header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './Header';
@@ -7,31 +9,34 @@ import Footer from './Footer';
 import Recipes from './Recipes';
 import Recipe from './Recipe';
 import Ingredients from './Ingredients';
+library.add(fab, fas);
 
 function App() {
   return (
     <div className="App container">
 
       <div className="row">
-        <Header/>
+        <Header />
       </div>
 
       <div className="row">
         <div className="col-md-3">
-          <Recipes/>
+          <Recipes />
         </div>
         <div className="col-md-6">
           <Recipe />
         </div>
         <div className="col-md-3">
-          <Ingredients/>
+          <Ingredients />
         </div>
       </div>
 
       <div className="row">
-        <Footer/>
+        <Footer />
       </div>
     </div>
+
+
   );
 }
 
