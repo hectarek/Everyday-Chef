d10e16b11960d64977326298c2541580a1fdefde
Hector Gonzalez <hectarek13@gmail.com>
2020-08-12 10:02:23 -0400

    async in recipe

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 function Recipe() {
+
+    async function getRecipes() {
+        let response = await fetch(`https://forkify-api.herokuapp.com/api/search?q=pizza`);
+        let data = await response.json();
+        return data.recipes;
+    }
+
+    async function getRecipeTitle(i) {
+        let recipe = await getRecipes();
+        return recipe[i].title;
+        
+    }
+
   return (
     <div className="recipe">
-
+        <h2 className="recipe-title">{getRecipeTitle(3)}</h2>
     </div>
   );
 }
