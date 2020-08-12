78118076ddb45263314191ee3aa8b23f1db3f582
John Yerger <johnyerger@johns-mbp.home>
2020-08-12 09:56:44 -0400

    CSS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Header from './Header';
-import Footer from './Footer';
-import Recipes from './Recipes';
-import Recipe from './Recipe';
-import Ingredients from './Ingredients';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./../style/App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Recipes from "./Recipes";
+import Recipe from "./Recipe";
+import Ingredients from "./Ingredients";
 
 function App() {
   return (
-    <div className="App container">
-
-      <div className="row">
-        <Header/>
+    <div className="app-container">
+      <div id="header-div" className="row">
+        <h1 id="logo">
+          Everyday <span>Chef</span>
+        </h1>
+        <Header />
       </div>
-
       <div className="row">
-        <div className="col-md-3">
-          <Recipes/>
+        <div id="recipes-div" className="col-md-3">
+          <h1>Recipes</h1>
+          <Recipes />
         </div>
-        <div className="col-md-6">
+        <div id="middle-div" className="col-md-6">
+          <h1>Middle</h1>
           <Recipe />
         </div>
-        <div className="col-md-3">
-          <Ingredients/>
+        <div id="ingredients-div" className="col-md-3">
+          <h1>Ingredients</h1>
+          <Ingredients />
         </div>
       </div>
-
-      <div className="row">
-        <Footer/>
+      <div id="footer-div" className="row">
+        <h1>Footer</h1>
+        <Footer />
       </div>
     </div>
   );
