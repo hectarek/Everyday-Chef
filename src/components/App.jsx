697eff4c0176942f731ae6d31acb232ba2fa19bb
John Yerger <johnyerger@johns-mbp.home>
2020-08-12 14:37:18 -0400

    again

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
@@ -11,24 +10,10 @@ import Footer from "./Footer";
 import Recipes from "./Recipes";
 import Recipe from "./Recipe";
 import Ingredients from "./Ingredients";
-=======
-import React from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Header from './Header';
-import Footer from './Footer';
-import Recipes from './Recipes';
-import Recipe from './Recipe';
-import Ingredients from './Ingredients';
->>>>>>> footer
 library.add(fab, fas);
 
 function App() {
   return (
-<<<<<<< HEAD
     <div className="app-container">
       <div
         id="header-div"
@@ -36,50 +21,25 @@ function App() {
       >
         <Header />
       </div>
-      <div
-        id="main-row"
-        className="row d-flex justify-content-center text-center"
-      >
-        <div
-          id="recipes-div"
-          className="col-md-3 position-relative container-fluid"
-        >
+      <div className="row d-flex justify-content-center text-center">
+        <div id="recipes-div" className="col-md-3">
           <h1>Recipes</h1>
-=======
-    <div className="App container">
-
-      <div className="row">
-        <Header />
-      </div>
-      <div className="row">
-        <div className="col-md-3">
->>>>>>> footer
           <Recipes />
         </div>
-        <div
-          id="middle-div"
-          className="col-md-3 position-relative container-fluid"
-        >
+        <div id="middle-div" className="col-md-6">
           <h1>Middle</h1>
           <Recipe />
         </div>
-        <div
-          id="ingredients-div"
-          className="col-md-3 position-relative container-fluid"
-        >
+        <div id="ingredients-div" className="col-md-3">
           <h1>Ingredients</h1>
           <Ingredients />
         </div>
       </div>
-<<<<<<< HEAD
       <div
         id="footer-div"
         className="row d-flex justify-content-center text-center"
       >
         <h1>Footer</h1>
-=======
-      <div id="footer-div" className="row">
->>>>>>> footer
         <Footer />
       </div>
     </div>
