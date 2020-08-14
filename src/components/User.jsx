7b21001e187cd5c44d62b3647bd75a4da4471e3d
ashwini82 <ashwiniatharv1982@gmail.com>
2020-08-14 16:56:56 -0400

    adding user

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,35 +2,77 @@ import React from "react";
 
 function User() {
     return (
-        <div className="user">
-            <div className="container">
+        <div class="container">
+            <div class="row">
 
-                <div className="row">
-                    {/* side bar       */}
-                    <div class="col-sm-4">
+                {/* <!--Details info--> */}
+                <div class="col-sm-12 mt-5">
+                    <div class="row">
+                        <div class="col-sm-3">
 
-
+                            <h6>Username</h6>
+                        </div>
+                        <div class="col-sm-7">
+                            <h4>Ash</h4>
+                        </div>
+                        <div class="col-sm-2">
+                            <h7>Edit</h7>
+                        </div>
                     </div>
-                    {/*main page */}
-                    <div class="col-sm-8">
 
+                    <hr />
+                    {/* <!--section2--> */}
+                    <div class="row">
+                        <div class="col-sm-3">
 
-
-
+                            <h6>Email</h6>
+                        </div>
+                        <div class="col-sm-7">
+                            <h7>adc@gmail.com</h7>
+                        </div>
+                        <div class="col-sm-2">
+                            <h7>Edit</h7>
+                        </div>
                     </div>
+                    <hr>
+                    </hr>
+                    {/* <!--section3--> */}
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6>Favorites</h6>
 
+                        </div>
+                        <div class="col-sm-7">
+                            <ul>
+                                <li>Pizza</li>
+                                <li>Pasta</li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-2">
+                            <h7>Edit</h7>
+                        </div>
+                    </div>
+                    <hr>
+                    </hr>
+                    {/* <!--section4--> */}
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6>Delete</h6>
 
-
+                        </div>
+                        <div class="col-sm-7">
+                            <p>Check details</p>
+                        </div>
+                        <div class="col-sm-2">
+                            <h6>Edit</h6>
+                        </div>
+                        <hr>
+                        </hr>
+                    </div>
 
                 </div>
-
-
-
             </div>
-
-
         </div>
-    );
 
+    );
 }
-export default User;
