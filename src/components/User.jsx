e36122acc07e1d9f6d5f49b53de1a9013addec68
ashwini82 <ashwiniatharv1982@gmail.com>
2020-08-14 15:26:24 -0400

    adding files

---
+++ src/components/User.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function User() {
+    return (
+        <div className="user">
+            <div className="container">
+
+                <div className="row">
+                    {/* side bar       */}
+                    <div class="col-sm-4">
+
+
+                    </div>
+                    {/*main page */}
+                    <div class="col-sm-8">
+
+
+
+
+                    </div>
+
+
+
+
+                </div>
+
+
+
+            </div>
+
+
+        </div>
+    );
+
+}
+export default User;
