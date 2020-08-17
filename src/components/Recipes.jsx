962c87945dd67db1231853da2609f0f06a55277b
John Yerger <johnyerger@johns-mbp.home>
2020-08-17 11:16:28 -0400

    push

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import "../style/Recipes.css"
+import "../style/Recipes.css";
 
 function Recipes({ recipe }) {
-
   return (
     <div className="recipes">
-        <div className="recipes-container">
-          <div className="recipes-image-container">
-            <img className="recipes-image" src={recipe.image} alt={recipe.label} />
-          </div>
-          <div className="recipes-text-container">
-            <h2 className="recipes-title">{recipe.label}</h2>
-            <h3 className="recipes-publisher">{recipe.source}</h3>
-          </div>
+      <div className="recipes-container">
+        <div className="recipes-image-container">
+          <img
+            className="recipes-image"
+            src={recipe.image}
+            alt={recipe.label}
+          />
         </div>
+        <div className="recipes-text-container">
+          <h2 className="recipes-title">{recipe.label}</h2>
+          <h3 className="recipes-publisher">{recipe.source}</h3>
+        </div>
+      </div>
     </div>
   );
 }
